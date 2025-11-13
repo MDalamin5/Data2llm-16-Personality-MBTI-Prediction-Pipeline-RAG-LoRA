b095441c76ddf005f0ebb2bf5c726de7cb962664
mdalamin5 <mdal.amin5@northsouth.edu>
2025-11-13 17:45:41 +0600

    add guardrails in the rag input and output

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-from rag_pipeline import create_vector_store, create_rag_chain
+from rag_pipeline import create_vector_store, create_rag_chain, initialize_models
+import nest_asyncio
+from nemoguardrails import RailsConfig
+from nemoguardrails.integrations.langchain.runnable_rails import RunnableRails
 
 if __name__ == "__main__":
+    nest_asyncio.apply()
+    llm, _ = initialize_models()
     # Create the vector store from the documents
     vector_store = create_vector_store()
 
@@ -9,7 +14,11 @@ if __name__ == "__main__":
 
     # Create and run the RAG chain
     rag_chain = create_rag_chain(vector_store, user_query)
-    result = rag_chain.invoke(user_query)
+    config = RailsConfig.from_path("config")
+    guard_rail = RunnableRails(config=config, llm=llm)
+    guard_with_rag_chain = guard_rail | rag_chain
+
+    result = guard_with_rag_chain.invoke(user_query)
 
     # Print the result
     print(result)
